b6e42311b75564d6ec2cb9424be9b304ac237325
jackson <jackson@zeroninelab.com>
2012-06-18 16:45:25 -0700

    azure deploy

--- application/views/facebook_view.php
+++ application/views/facebook_view.php
@@ -74,36 +74,32 @@
 		</div>
 		
 <div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '329330017143635', // App ID
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
 
-    // Additional initialization code here
-  };
-
-    // Load the SDK Asynchronously
-    (function(d){
-       var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-       js = d.createElement('script'); js.id = id; js.async = true;
-       js.src = "//connect.facebook.net/en_US/all.js";
-       d.getElementsByTagName('head')[0].appendChild(js);
-     }(document));
-
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<%=FB_APP_ID%>";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-  </script>
-</script>
+		<script>
+		  window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '329330017143635', // App ID
+		        status     : true,
+		        cookie     : true,
+		        oauth      : true,
+		        xfbml      : true
+		      });
+		  };
+		  // Load the SDK Asynchronously
+		  (function(d){
+		    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+		    js = d.createElement('script'); js.id = id; js.async = true;
+		    js.src = "//connect.facebook.net/en_US/all.js";
+		    d.getElementsByTagName('head')[0].appendChild(js);
+		  }(document));
+		  (function(d, s, id) {
+		    var js, fjs = d.getElementsByTagName(s)[0];
+		    if (d.getElementById(id)) {return;}
+		    js = d.createElement(s); js.id = id;
+		    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<%=FB_APP_ID%>";
+		    fjs.parentNode.insertBefore(js, fjs);
+		  }(document, 'script', 'facebook-jssdk'));
+		</script>
 
 	</body>
 </html>
