0797d07248e5e3b116541bb4e4208176042aadb3
jackson <jackson@zeroninelab.com>
2012-06-18 15:11:19 -0700

    azure deploy

--- application/controllers/core.php
+++ application/controllers/core.php
@@ -9,29 +9,6 @@ class Core extends CI_Controller {
 
     function index()
     {
-        $this->load->config('facebook'); 
-        $fb_config = array(
-            'appId'  => '329330017143635',
-            'secret' => '4a3cae68535777a6793df72ab18c7904' 
-        );
-
-        $this->load->library('facebook', $fb_config);
-
-        $user = $this->facebook->getUser();
-
-        if ($user) {
-            try {
-                $data['user_profile'] = $this->facebook->api('/me');
-            } catch (FacebookApiException $e) {
-                $user = null;
-            }
-        }
-
-        if ($user) {
-            $data['logout_url'] = $this->facebook->getLogoutUrl();
-        } else {
-            $data['login_url'] = $this->facebook->getLoginUrl();
-        }
 
         $this->load->view('core',$data);
     }
--- application/views/core.php
+++ application/views/core.php
@@ -5,18 +5,7 @@
 <body>
     <h1>Facebook stuff</h1>
 
-    <?php if (@$user_profile): ?>
-        <pre>
-            <?php echo print_r($user_profile, TRUE) ?>
-        </pre>
-        <a href="<?php echo $logout_url ?>">Logout of this thing</a>
-    <?php else: ?>
-        <h2>Welcome to this facebook thing, please login below</h2>
-        <a href="<?php echo $login_url ?>">Login to this thing</a>
-         <pre>
-            <?php echo print_r($user_profile, TRUE) ?>
-        </pre>
-    <?php endif; ?>
+    this is core
 
 </body>
 
