33fd37e1f7c915f960631068a2b354fa95dd459a
jackson <jackson@zeroninelab.com>
2012-06-18 13:43:10 -0700

    added FB app config

---
+++ application/controllers/core.php
@@ -0,0 +1,38 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Core extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+
+        $fb_config = array(
+            'appId'  => 'YOUR_APP_ID_HERE',
+            'secret' => 'YOUR_APP_SECRET_HERE'
+        );
+
+        $this->load->library('facebook', $fb_config);
+
+        $user = $this->facebook->getUser();
+
+        if ($user) {
+            try {
+                $data['user_profile'] = $this->facebook->api('/me');
+            } catch (FacebookApiException $e) {
+                $user = null;
+            }
+        }
+
+        if ($user) {
+            $data['logout_url'] = $this->facebook->getLogoutUrl();
+        } else {
+            $data['login_url'] = $this->facebook->getLoginUrl();
+        }
+
+        $this->load->view('view',$data);
+    }
+}
