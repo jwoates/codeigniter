fbaceb32d51688f7ea811f52713d6eb64e306c20
jackson <jackson@zeroninelab.com>
2012-06-18 17:11:43 -0700

    azure deploy

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,7 @@
 <?php
 
+  $config['facebook_app_url']         = 'https://apps.facebook.com/azure-app/';
 	$config['facebook_app_id'] 			   = '329330017143635';
-	$config['facebook_api_secret'] 		 = '4a3cae68535777a6793df72ab18c7904';
+  $config['facebook_api_secret']     = '4a3cae68535777a6793df72ab18c7904';
 	$config['facebook_default_scope']	 = 'email,user_photos'; // E.G 'read_stream,birthday,users_events,rsvp_event'
 	$config['facebook_api_url'] 		   = 'https://graph.facebook.com/'; // Just in case it changes.
--- application/views/facebook_auth.php
+++ application/views/facebook_auth.php
@@ -1,3 +1,4 @@
 <script>
-  top.location.href = '<?php echo site_url('facebook_test')?>';
+  // top.location.href = '<?php echo site_url('facebook_test')?>';
+  top.location.href = 'https://apps.facebook.com/azure-app/';
 </script>
