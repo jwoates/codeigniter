36eafba244896c8f798d6e72217bacf7ed80cac5
jackson <jackson@zeroninelab.com>
2012-06-26 16:11:39 -0700

    azure deploy

--- application/views/agegate.php
+++ application/views/agegate.php
@@ -8,14 +8,33 @@
       echo $message;
     }else{
       echo form_open('agegate/authenticate');
-        $data = array(
-          'name'        => 'age',
-          'id'          => 'age',
-          'value'       => 'enter age',
-          'maxlength'   => '3',
+        $day = array(
+          'name'        => 'day',
+          'id'          => 'day',
+          'value'       => 'DD',
+          'maxlength'   => '2',
           'size'        => '10'
         );
-      echo form_input($data);
+      echo form_input($day);
+
+        $month = array(
+          'name'        => 'month',
+          'id'          => 'month',
+          'value'       => 'MM',
+          'maxlength'   => '2',
+          'size'        => '10'
+        );
+      echo form_input($month);
+
+        $year = array(
+          'name'        => 'year',
+          'id'          => 'year',
+          'value'       => 'YYYY',
+          'maxlength'   => '4',
+          'size'        => '10'
+        );
+      echo form_input($year);      
+
       echo form_submit('submit', 'Enter');
       echo form_close();
     }
