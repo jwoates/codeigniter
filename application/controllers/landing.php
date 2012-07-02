b23f536f80a01bf624f6d381fb03b680b340c055
jackson <jackson@zeroninelab.com>
2012-07-02 10:51:14 -0700

    ready for page integration

--- application/controllers/landing.php
+++ application/controllers/landing.php
@@ -38,7 +38,7 @@ class Landing extends CI_Controller {
         $this->zend->load('Zend/Gdata/YouTube');
         $this->yt = new Zend_Gdata_YouTube();
         $this->yt->setMajorProtocolVersion(2);
-        $url = 'http://gdata.youtube.com/feeds/api/playlists/1043609265A2F46F?v=2';
+        $url = 'http://gdata.youtube.com/feeds/api/playlists/PL0A653148CC3DE1CD?v=2';
         $videoFeed = $this->yt->getPlaylistVideoFeed($url);
         // Print out metadata for each video in the playlist
         $pl_array = array();
