d01b76513398e1fdff7a1e881d9b31fb42480fa2
Vadim Goncharov <vadim@owl-design.net>
2012-07-11 10:50:32 -0700

    google analytics added

--- application/views/redirect/index.php
+++ application/views/redirect/index.php
@@ -1,4 +1,5 @@
 <script type="text/javascript">
   top.window.location = 'http://sdcc.azurewebsites.net/safaripass'
-  //top.window.location = 'http://google.com'
+
+  // top.window.location = 'http://google.com'
 </script>
